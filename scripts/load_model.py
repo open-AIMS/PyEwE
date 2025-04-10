b40dad9a41786063f6f5dcf4cff7cc04ba7f38e9
Daniel Tan <d.tan@aims.gov.au>
2025-04-10 17:01:33 +1000

    save ecotracer results

--- scripts/load_model.py
+++ scripts/load_model.py
@@ -1,20 +1,37 @@
-from decom_py import EwE
+import warnings
+from decom_py import EwE, EwEState
 
 ewe = EwE(r'C:\Program Files\Ecopath with Ecosim 40 years 6.7.0.18865_64-bit', 'Outputs/')
 core = ewe.core()
+state = EwEState(core)
 
-model_file = r'C:\Users\dtan\data\Decommissioning\Past_Ecopath\NorthSea\North Sea.eweaccdb'
+CORE = ewe._get_core_module()
+UTIL = ewe._get_util_module()
+
+model_file = r'C:\Users\dtan\data\Decommissioning\Past_Ecopath\GippslandBasin\East Bass Strait.eweaccdb'
 ewe.load_model(model_file)
+state.print_summary()
 
-ewe.run_ecopath()
 ewe.load_ecosim_scenario(1)
-ewe.load_ecotracer_scenario(1)
+
+if not ewe.load_ecotracer_scenario(1):
+    print("Failed to load ecotracer scenario.")
+if not core.ActiveEcotracerScenarioIndex == 1:
+    print("Didn't load ecotracer scenarios.")
 
 if not ewe.run_ecosim():
     print("Failed to run ecosim.")
+state.print_summary()
+
+print(core.EcosimModelParameters.ContaminantTracing)
+
+print(core.nEcosimYears)
+print(core.EcotracerModelParameters.CZero)
 
 ewe.save_ecopath_results()
-if not ewe.save_ecosim_results('Outputs/ecosim_res'):
-    print("Failed to save ecosim results.")
+ewe.save_ecosim_results('Outputs/ecosim_res')
+
+if not ewe.save_ecotracer_results():
+    print("Failed to save ecotracer results.")
 
 core.CloseModel()
--- src/decom_py/EwE.py
+++ src/decom_py/EwE.py
@@ -1,3 +1,4 @@
+import abc
 import clr
 import sys
 import os
@@ -17,13 +18,28 @@ class EwE:
         if not self._ewe_core_path.exists():
             raise FileNotFoundError(self._ewe_core_path)
 
+        self._ewe_util_path = self._ewe_dir.joinpath('EwEUtils.dll')
+        if not self._ewe_core_path.exists():
+            raise FileNotFoundError(self._ewe_core_path)
+
         clr.AddReference(str(self._ewe_core_path))
 
         self._ewe_core = import_module('EwECore')
+        self._ewe_util = import_module('EwEUtils')
         self._core = self._ewe_core.cCore()
         self._ecopath_result_writer = self._ewe_core.cEcopathResultWriter(self._core)
         self._ecosim_result_writer = self._ewe_core.Ecosim.cEcosimResultWriter(self._core)
+        self._ecotracer_result_writer = self._ewe_core.cEcotracerResultWriter(self._core)
         self._core.OutputPath = result_save_dir
+
+    def core(self):
+        return self._core
+
+    def _get_core_module(self):
+        return self._ewe_core
+
+    def _get_util_module(self):
+        return self._ewe_util
 
     def load_model(self, path: str):
         return self._core.LoadModel(path)
@@ -46,9 +62,6 @@ class EwE:
 
         return self._core.LoadEcotracerScenario(idx)
 
-    def core(self):
-        return self._core
-
     def run_ecopath(self):
         is_balanced: bool = self._core.IsModelBalanced
         if not is_balanced:
@@ -59,7 +72,7 @@ class EwE:
         return results
 
     def run_ecosim(self):
-    
+
         self.run_ecopath()
         successful: bool = self._core.RunEcosim()
 
@@ -71,4 +84,11 @@ class EwE:
 
     def save_ecosim_results(self, dir: str):
         # Missing use monthly enum type to pass to write results.
-        return self._ecosim_result_writer.WriteResults(dir)
+        if not self._ecosim_result_writer.WriteResults(dir):
+            warn("Failed to save ecosim results. Make sure target directory is empty.")
+            return False
+
+        return True
+
+    def save_ecotracer_results(self):
+        return self._ecotracer_result_writer.WriteEcosimResults()
