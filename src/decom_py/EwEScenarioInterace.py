1e5b2e2becd32b534818daf0d0488b8f88f10cc9
Daniel Tan <d.tan@aims.gov.au>
2025-04-15 16:39:01 +1000

    update scenario interface constructor to copy model

--- src/decom_py/EwEScenarioInterace.py
+++ src/decom_py/EwEScenarioInterace.py
@@ -1,22 +1,48 @@
 from pandas import DataFrame
+from pathlib import Path
+from tempfile import TemporarDirectory, TemporaryDirectory
+import shutil
+import os
 
 from decom_py import CoreInterface
-from pathlib import Path
-
 from Exceptions import EwEError
 
 class EwEScenarioInterface:
 
     def __init__(self, model_path: str):
-
-        if not Path(model_path).exists():
+        mod_path_obj = Path(model_path)
+        if not mod_path_obj.exists():
             raise FileNotFoundError(model_path)
+
+        # The temporary directory should clean itself up.
+        self._temp_dir = TemporaryDirectory()
+        self._temp_model_path = os.path.join(self._temp_dir.name, os.path.basename(model_path))
+
+        # to avoid modifying the original model file, create a copy
+        shutil.copy2(model_path, self.temp_model_path)
 
         self._core_instance = CoreInterface()
         if not self._core_instance.load_model(model_path):
             msg = "Failed to load EwE model. "
             msg += "Check that the model file is loadable via the gui."
             raise EwEError(self._core_instance.get_state(), msg)
+
+        self._core_instance.new_ecosim_scenario(
+            "tmp_ecosim_scen",
+            "temporary ecosim scenario used by decom_py",
+            "", # author
+            ""  # contact
+        )
+
+        self._core_instance.new_ecotracer_scenario(
+            "tmp_ecotracer_scen",
+            "temporary ecosim scenario used by decom_py",
+            "", # author
+            ""  # contact
+        )
+
+    def get_temp_model_path(self):
+        return self._temp_model_path
 
     def run_scenarios(self, scenarios: DataFrame):
         return None
